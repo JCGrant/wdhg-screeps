8b8f819c2cc2541d80fc50026b05e42bdd57bb46
wdhg <wdhgrant@gmail.com>
2017-09-21 16:43:40 +0100

    remove broken code

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -3,12 +3,6 @@ const utils = require("utils");
 const roleHarvester = {
     run: function(creep) {
         
-        if(creep.memory.targetContainer) {
-            console.log(creep.memory.targetContainer);
-            const sources = creep.room.find(FIND_SOURCES);
-            creep.moveTo(creep.memory.targetContainer);
-            creep.harvest(sources[0]);
-        }
 	}
 };
 
